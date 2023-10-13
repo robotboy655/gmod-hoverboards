9289e25a4f2346c75d11b0bf5ed60b54dce64283
Rubat <3299036+robotboy655@users.noreply.github.com>
2023-10-14 01:15:25 +0300

    Update _update.bat

---
+++ _update.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: Must have gmad.bat and gmpublish.bat on PATH like so: gmpublish.exe %*
+
+echo.
+
+set id=150455514
+
+echo WORKSHOP ID IS %id%
+
+echo.
+echo Creating GMA
+call gmad create -folder %cd% -out %id%.gma -quiet
+
+echo.
+echo Uploading GMA
+call gmpublish update -addon %id%.gma -id %id%
+
+echo.
+echo Deleting GMA
+del %id%.gma
+
+pause
